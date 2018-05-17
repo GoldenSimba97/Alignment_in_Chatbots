23513d3bc06fab8c6c2fc93139734687241f2b07
Kim Gouweleeuw <kim.gouweleeuw@gmail.com>
2018-05-17 20:21:15 +0200

    Removed unnecessary code

--- alignment.py
+++ alignment.py
@@ -5,17 +5,18 @@ import sys
 from collections import defaultdict
 import numpy as np
 
-try:
-	import cPickle as pickle
-except:
-	import pickle
+# try:
+# 	import cPickle as pickle
+# except:
+# 	import pickle
 
 class QRPair:
-	def __init__(self, key, discussion_id, response_post_id, quote_post_id):
+	def __init__(self, key):
+	# def __init__(self, key, discussion_id, response_post_id, quote_post_id):
 		self.key = key
-		self.discussion_id = discussion_id
-		self.response_post_id = response_post_id
-		self.quote_post_id = quote_post_id
+		# self.discussion_id = discussion_id
+		# self.response_post_id = response_post_id
+		# self.quote_post_id = quote_post_id
 
 
 	def add_quote(self, quote):
@@ -30,38 +31,38 @@ class QRPair:
 		'''
 		self.response = response
 
-	def add_annotations(self, annotations):
-		'''
-		Add dictionary of annotations to object,
-		get agreement
-		'''
-		self.annotations = annotations
-		self.agreement = annotations['agreement']
-		self.sarcasm = annotations['sarcasm']
-		self.nicenasty = annotations['nicenasty']
+	# def add_annotations(self, annotations):
+	# 	'''
+	# 	Add dictionary of annotations to object,
+	# 	get agreement
+	# 	'''
+	# 	self.annotations = annotations
+	# 	self.agreement = annotations['agreement']
+	# 	self.sarcasm = annotations['sarcasm']
+	# 	self.nicenasty = annotations['nicenasty']
 
 
-	def update_authorratio(self, author_ratio_dict):
-		"""
-		Update dictionary that saves ratio of disagreement and agreement responses for response author
-		"""
-
-		if float(self.agreement) > 1.0:
-			agreement = 'agreement'
-		elif float(self.agreement) > -1.0:
-			agreement = 'disagreement'
-		elif float(self.agreement) <= 1.0 or float(self.agreement) >= -1.0:
-			agreement = 'neutral'
-		# invalid agreement level?
-		else:
-			return author_ratio_dict
-
-		try:
-			author_ratio_dict[self.response_author][agreement] += 1
-		except KeyError:
-			author_ratio_dict[self.response_author][agreement] = 1
-
-		return author_ratio_dict
+	# def update_authorratio(self, author_ratio_dict):
+	# 	"""
+	# 	Update dictionary that saves ratio of disagreement and agreement responses for response author
+	# 	"""
+    #
+	# 	if float(self.agreement) > 1.0:
+	# 		agreement = 'agreement'
+	# 	elif float(self.agreement) > -1.0:
+	# 		agreement = 'disagreement'
+	# 	elif float(self.agreement) <= 1.0 or float(self.agreement) >= -1.0:
+	# 		agreement = 'neutral'
+	# 	# invalid agreement level?
+	# 	else:
+	# 		return author_ratio_dict
+    #
+	# 	try:
+	# 		author_ratio_dict[self.response_author][agreement] += 1
+	# 	except KeyError:
+	# 		author_ratio_dict[self.response_author][agreement] = 1
+    #
+	# 	return author_ratio_dict
 
 
 	def update_authordict(self, author_dict, response_dict):
@@ -165,23 +166,23 @@ class QRPair:
 		return int(total_markers)
 
 
-	def get_author_by_id(self,post_id):
-		'''
-		Retrieve the author's username, using
-		the discussion and post id and the corresponding
-		json files.
-		'''
-		author = ""
-		if self.discussion_id != "discussion_id":
-			json_data=open('../data/fourforums/discussions/'+ self.discussion_id +".json").read()
-			data = json.loads(json_data)
-			for post in data[0]:
-				try:
-					if int(post[0]) == int(post_id):
-						author = post[2]
-				except ValueError:
-					continue
-		return author
+	# def get_author_by_id(self,post_id):
+	# 	'''
+	# 	Retrieve the author's username, using
+	# 	the discussion and post id and the corresponding
+	# 	json files.
+	# 	'''
+	# 	author = ""
+	# 	if self.discussion_id != "discussion_id":
+	# 		json_data=open('../data/fourforums/discussions/'+ self.discussion_id +".json").read()
+	# 		data = json.loads(json_data)
+	# 		for post in data[0]:
+	# 			try:
+	# 				if int(post[0]) == int(post_id):
+	# 					author = post[2]
+	# 			except ValueError:
+	# 				continue
+	# 	return author
 
 def other_baseline(author, features, author_dict, response_dict):
 	try:
@@ -329,152 +330,129 @@ def getWords(post_text):
 	return text2
 
 
-def read_csv_data(features, smoothing, weight_vector, alternative="matching"):
-	'''
-	Adds all qr_pairs in qr_meta to a dictionary
-	of qr_pair objects, including their annotations.
-	'''
-	pair_dictionary = {}
-	#Get meta-data:
-	author_dict = {}
-	response_dict = {}
-
-	# list to save qrpairs to update on alignment later on
-	qrpair_list = []
-	# Loop through data to get all qrpairs
-
-	pickle_dict = {}
-
-	print "Reading meta data..."
-	with open('../data/fourforums/annotations/mechanical_turk/qr_meta.csv', 'rb') as meta_file:
-		reader = csv.reader(meta_file, delimiter=',', quotechar='"')
-		count = 0
-		for row in reader:
-			#if count > 10:
-				#break
-			key = row[0]
-			discussion_id = row[1]
-			response_post_id = row[2]
-			quote_post_id = row[3]
-			quote = row[8]
-			response = row[9]
-			qrpair = QRPair(key, discussion_id, response_post_id, quote_post_id)
-			qrpair.add_quote(quote)
-			qrpair.add_response(response)
-			if discussion_id != "discussion_id":
-				author_dict, other_author_dict, response_dict = qrpair.update_authordict( author_dict, response_dict)
-				#qrpair_list.append(qrpair)
-			pair_dictionary[str(key) + str(discussion_id)] = qrpair
-			count +=1
-
-			# save to dict to pickle for use in other scripts
-			pickle_dict[str(key) + str(discussion_id)] = [qrpair.quote, qrpair.response]
-
-	pickle_dump_qrpair(pickle_dict)
-
-	# Loop through all qrpairs again to compute alignment
-	for k, qrpair in pair_dictionary.iteritems():
-		# some qrpairs do not have the author dict and response dict
-		try:
-			qrpair.compute_alignment(features, author_dict, other_author_dict, response_dict, smoothing, weight_vector, alternative)
-		except:
-			pass
-
-	author_ratio_dict = defaultdict(lambda : defaultdict(int))
-
-	#Get annotation data:
-	print "Reading annotation data..."
-	with open('../data/fourforums/annotations/mechanical_turk/qr_averages.csv', 'rb') as average_file:
-		reader = csv.reader(average_file, delimiter=',', quotechar='"')
-		for row in reader:
-			try:
-				corresponding_pair = pair_dictionary[str(row[0]) + str(row[1])]
-				annotations = row[2:]
-				annotations_dictionary = list_to_dictionary(annotations)
-				corresponding_pair.add_annotations(annotations_dictionary)
-				# update ratio of number of agreeing/disagreeing posts by response author
-				try:
-					author_ratio_dict = corresponding_pair.update_authorratio(author_ratio_dict)
-				# sometimes qrpair does not contain a response author, so pass
-				except:
-					pass
-			except KeyError:
-				pass
-
-
-	return pair_dictionary, author_dict, response_dict, author_ratio_dict
-
-
-def pickle_dump_qrpair(pickle_dict):
-	"""
-	Dump qrpairs to file
-	key: str(key) + str(discussion_id),  value: [quote_text, response_text]
-	"""
-	pickle.dump( pickle_dict, open( "qr_texts.pickle", "wb" ) )
-
-def list_to_dictionary(annotations):
-	'''
-	Turns a row of annotation values from qr_averages into a dictionary for
-	easy annotation lookup.
-	'''
-	key_list = ["agree-disagree","agreement","agreement_unsure","attack","attack_unsure","defeater-undercutter","defeater-undercutter_unsure","fact-feeling","fact-feeling_unsure","negotiate-attack","negotiate-attack_unsure","nicenasty","nicenasty_unsure","personal-audience","personal-audience_unsure","questioning-asserting","questioning-asserting_unsure","sarcasm","sarcasm_unsure"]
-	annotation_dictionary = {}
-	for i in range(0, len(key_list)):
-		annotation_dictionary[key_list[i]] = annotations[i]
-	return annotation_dictionary
-
-def group_by_agreement(pair_dictionary):
-	"""
-	note: agreement is set on an 11 point scale [-5,5] with a slider
-	"""
-	agreement_list = []
-	disagreement_list = []
-	for pair, value in pair_dictionary.iteritems():
-		try:
-			if float(value.agreement) >= 0 :
-				#print "agree", value.agreement
-				agreement_list.append(value)
-			else:
-				#print "disagree", value.agreement
-				disagreement_list.append(value)
-		except ValueError:
-			continue
-	return agreement_list, disagreement_list
-
-def write_data(pair_dictionary,features, feature_name, author_dict, response_dict, author_ratio_dict, smoothing, weight_vector, alternative="matching"):
-	f = open(os.path.join('data_final_altweight/', feature_name +".txt"), 'w')
-	print "Writing ", feature_name, "to file"
-	for qr_pair in pair_dictionary:
-		pair = pair_dictionary[qr_pair]
-		if pair.discussion_id != 'discussion_id':
-			# ALREADY DONE IN READ CSV DATA?
-			#author_dict = pair.compute_alignment(features, author_dict, response_dict, smoothing, alternative)
-			number_posts = author_dict[pair.response_author]["number_posts"]
-			if (pair.alignment > 1.0 or pair.alignment < -1.0) and pair.alignment is not None:
-				print "alignment too high/low"
-				#sys.exit()
-
-			# calculate ratio of response author responses
-			ratiovalues = author_ratio_dict[pair.response_author]
-			total_ratio = float(sum(ratiovalues.values()))
-			neutral_ratio = ratiovalues['neutral']/total_ratio
-			disgagreement_ratio = ratiovalues['disagreement']/total_ratio
-			agreement_ratio = ratiovalues['agreement']/total_ratio
-
-			#if int(number_posts) == 0:
-			#	print "number_posts", number_posts
-			f.write(str(pair.key) + str(pair.discussion_id) + ";" + str(pair.agreement) + " ; " + str(pair.sarcasm) + " ; " + str(pair.nicenasty) + " ; " + str(pair.alignment) + " ; " + str(number_posts) + " ; " + str(pair.quote_length) + " ; " + str(pair.response_length)+ " ; " + str(agreement_ratio)+ " ; " + str(disgagreement_ratio)+ " ; " + str(neutral_ratio)  + "\n")
-	f.close()
+# def read_csv_data(features, smoothing, weight_vector, alternative="matching"):
+# 	'''
+# 	Adds all qr_pairs in qr_meta to a dictionary
+# 	of qr_pair objects, including their annotations.
+# 	'''
+# 	pair_dictionary = {}
+# 	#Get meta-data:
+# 	author_dict = {}
+# 	response_dict = {}
+#
+# 	# list to save qrpairs to update on alignment later on
+# 	qrpair_list = []
+# 	# Loop through data to get all qrpairs
+#
+# 	pickle_dict = {}
+#
+# 	print "Reading meta data..."
+# 	with open('../data/fourforums/annotations/mechanical_turk/qr_meta.csv', 'rb') as meta_file:
+# 		reader = csv.reader(meta_file, delimiter=',', quotechar='"')
+# 		count = 0
+# 		for row in reader:
+# 			#if count > 10:
+# 				#break
+# 			key = row[0]
+# 			discussion_id = row[1]
+# 			response_post_id = row[2]
+# 			quote_post_id = row[3]
+# 			quote = row[8]
+# 			response = row[9]
+# 			qrpair = QRPair(key, discussion_id, response_post_id, quote_post_id)
+# 			qrpair.add_quote(quote)
+# 			qrpair.add_response(response)
+# 			if discussion_id != "discussion_id":
+# 				author_dict, other_author_dict, response_dict = qrpair.update_authordict( author_dict, response_dict)
+# 				#qrpair_list.append(qrpair)
+# 			pair_dictionary[str(key) + str(discussion_id)] = qrpair
+# 			count +=1
+#
+# 			# save to dict to pickle for use in other scripts
+# 			pickle_dict[str(key) + str(discussion_id)] = [qrpair.quote, qrpair.response]
+#
+# 	pickle_dump_qrpair(pickle_dict)
+#
+# 	# Loop through all qrpairs again to compute alignment
+# 	for k, qrpair in pair_dictionary.iteritems():
+# 		# some qrpairs do not have the author dict and response dict
+# 		try:
+# 			qrpair.compute_alignment(features, author_dict, other_author_dict, response_dict, smoothing, weight_vector, alternative)
+# 		except:
+# 			pass
+#
+# 	author_ratio_dict = defaultdict(lambda : defaultdict(int))
+#
+# 	#Get annotation data:
+# 	print "Reading annotation data..."
+# 	with open('../data/fourforums/annotations/mechanical_turk/qr_averages.csv', 'rb') as average_file:
+# 		reader = csv.reader(average_file, delimiter=',', quotechar='"')
+# 		for row in reader:
+# 			try:
+# 				corresponding_pair = pair_dictionary[str(row[0]) + str(row[1])]
+# 				annotations = row[2:]
+# 				annotations_dictionary = list_to_dictionary(annotations)
+# 				corresponding_pair.add_annotations(annotations_dictionary)
+# 				# update ratio of number of agreeing/disagreeing posts by response author
+# 				try:
+# 					author_ratio_dict = corresponding_pair.update_authorratio(author_ratio_dict)
+# 				# sometimes qrpair does not contain a response author, so pass
+# 				except:
+# 					pass
+# 			except KeyError:
+# 				pass
+#
+#
+# 	return pair_dictionary, author_dict, response_dict, author_ratio_dict
 
 
-def write_data_qrtext(pair_dictionary,features, feature_name):
-	f = open(os.path.join('data/', "withtext_" + feature_name +".txt"), 'w')
-	for qr_pair in pair_dictionary:
-		pair = pair_dictionary[qr_pair]
-		if pair.discussion_id != 'discussion_id':
-			pair.compute_alignment(features)
-			f.write(str(pair.quote.split()) + " ; " + str(pair.response.split()) + " ; " + str(pair.agreement) + " ; " + str(pair.sarcasm) + " ; " + str(pair.nicenasty) + " ; " + str(pair.alignment) + "\n")
-	f.close()
+# def pickle_dump_qrpair(pickle_dict):
+# 	"""
+# 	Dump qrpairs to file
+# 	key: str(key) + str(discussion_id),  value: [quote_text, response_text]
+# 	"""
+# 	pickle.dump( pickle_dict, open( "qr_texts.pickle", "wb" ) )
+
+# def list_to_dictionary(annotations):
+# 	'''
+# 	Turns a row of annotation values from qr_averages into a dictionary for
+# 	easy annotation lookup.
+# 	'''
+# 	key_list = ["agree-disagree","agreement","agreement_unsure","attack","attack_unsure","defeater-undercutter","defeater-undercutter_unsure","fact-feeling","fact-feeling_unsure","negotiate-attack","negotiate-attack_unsure","nicenasty","nicenasty_unsure","personal-audience","personal-audience_unsure","questioning-asserting","questioning-asserting_unsure","sarcasm","sarcasm_unsure"]
+# 	annotation_dictionary = {}
+# 	for i in range(0, len(key_list)):
+# 		annotation_dictionary[key_list[i]] = annotations[i]
+# 	return annotation_dictionary
+
+# def group_by_agreement(pair_dictionary):
+# 	"""
+# 	note: agreement is set on an 11 point scale [-5,5] with a slider
+# 	"""
+# 	agreement_list = []
+# 	disagreement_list = []
+# 	for pair, value in pair_dictionary.iteritems():
+# 		try:
+# 			if float(value.agreement) >= 0 :
+# 				#print "agree", value.agreement
+# 				agreement_list.append(value)
+# 			else:
+# 				#print "disagree", value.agreement
+# 				disagreement_list.append(value)
+# 		except ValueError:
+# 			continue
+# 	return agreement_list, disagreement_list
+
+
+
+
+# def write_data_qrtext(pair_dictionary,features, feature_name):
+# 	f = open(os.path.join('data/', "withtext_" + feature_name +".txt"), 'w')
+# 	for qr_pair in pair_dictionary:
+# 		pair = pair_dictionary[qr_pair]
+# 		if pair.discussion_id != 'discussion_id':
+# 			pair.compute_alignment(features)
+# 			f.write(str(pair.quote.split()) + " ; " + str(pair.response.split()) + " ; " + str(pair.agreement) + " ; " + str(pair.sarcasm) + " ; " + str(pair.nicenasty) + " ; " + str(pair.alignment) + "\n")
+# 	f.close()
 
 def get_features_from_file(file_name):
     """
@@ -502,10 +480,47 @@ if __name__=="__main__":
 			features = get_features_from_file('coordination_markers/'+mark+'.txt')
 		else:
 			features = [mark]
-		print "Reading data from file..."
-		pair_dictionary, author_dict,response_dict, author_ratio_dict = read_csv_data(features, smoothing, weight_vector, alternative="final")
+
+		responses_dict = {40.0: "You're making your point.", 50.0: 'Swearing is often cathartic.'}
+		# align = Align("you cunt, why won't you tell me something", responses_dict)
+		quote = "you cunt, why won't you tell me something"
+
+		# quote_post = Post(quote, "user")
+        # for key, response in responses_dict.items():
+        #     response_post = Post(response, "chatbot")
+        #     utterance = Utterance(quote_post, response_post, key)
+        #     author1 = Author(utterance.response_object.author)
+        #     self.author_dict[utterance.response_object.author] = author1
+        #     author2 = Author(utterance.quote_object.author)
+        #     self.author_dict[utterance.quote_object.author] = author2
+        #     author1.add_utterance(utterance)
+        #     author2.add_utterance(utterance)
+        #     self.utterance_dict[str(key)] = utterance
+
+		pair_dictionary = {}
+		for key, response in response_dict.items():
+			qrpair = QRPair(key)
+			qrpair.add_quote(quote)
+			qrpair.add_response(response)
+			pair_dictionary[str(key)] = qrpair
+
+		alignment_dict = {}
+		# Loop through all qrpairs again to compute alignment
+		for k, qrpair in pair_dictionary.items():
+			# some qrpairs do not have the author dict and response dict
+			try:
+				alignment_dict[k] = qrpair.compute_alignment(features, author_dict, other_author_dict, response_dict, smoothing, weight_vector, alternative="final")
+			except:
+				pass
+
+		# qrpair = QRPair(key)
+		# qrpair.add_quote(quote)
+		# qrpair.add_response(response)
+
+		# print "Reading data from file..."
+		# pair_dictionary, author_dict,response_dict, author_ratio_dict = read_csv_data(features, smoothing, weight_vector, alternative="final")
 		#print "KY", pair_dictionary.keys()
-		print "Getting agreement groups..."
-		agreements, disagreements = group_by_agreement(pair_dictionary)
-		print "writing to file for %s" %(mark)
-		write_data(pair_dictionary,features, mark, author_dict, response_dict, author_ratio_dict, smoothing, weight_vector, alternative="final")
+		# print "Getting agreement groups..."
+		# agreements, disagreements = group_by_agreement(pair_dictionary)
+		# print "writing to file for %s" %(mark)
+		# write_data(pair_dictionary,features, mark, author_dict, response_dict, author_ratio_dict, smoothing, weight_vector, alternative="final")
